4d0dd610e6303383bedf6db3ebed02e613e9b8c3
reann <reann.corrine05@gmail.com>
2023-11-24 23:47:42 -0800

    movement is good, working on pew pew laser now

--- player.gd
+++ player.gd
@@ -1,27 +1,26 @@
-extends 
+extends CharacterBody2D
 
+var gravity = 7
+var speed = 300
 
-#var gravity = 7
-#var speed = 60
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	if Input.is_action_pressed("ui_right"):
-#		$QueenSprite.play("walk")
-#		$QueenSprite.flip_h = false
-#		velocity.x += -speed
-#	elif Input.is_action_pressed("ui_left"):
-#		velocity.x += speed
-#		$QueenSprite.play("walk") 
-#		$QueenSprite.flip_h = true 
-#	else:
-#		velocity.x = 0
-#		$QueenSprite.play("idle")
-#
-#	velocity.y += gravity 
-#
-#	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-#		velocity.y = -200
-#
-#	move_and_slide()
-#
-#
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -speed 
+		$QueenSprite.play("walkingqueen")
+		$QueenSprite.flip_h = true 
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = speed 
+		$QueenSprite.play("walkingqueen")
+		$QueenSprite.flip_h = false
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -speed 
+		$QueenSprite.play("jumpingqueen")
+	else:
+		velocity.x = 0
+		$QueenSprite.play("idlequeen")
+		$QueenSprite.flip_h = false
+		
+	velocity.y += gravity 
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -120
+	move_and_slide()
